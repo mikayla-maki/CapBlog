2f5af835890b6d8179c0378354578f841def44c1
Mikayla Maki <mikayla.c.maki@gmail.com>
2022-10-30 11:09:44 -0700

    Almost done. have the loggable / revocable proxy built :D

--- src/heart-of-spritely.rkt
+++ src/heart-of-spritely.rkt
@@ -3,6 +3,8 @@
 (require goblins)
 (require goblins/actor-lib/bootstrap)
 (require goblins/actor-lib/methods)
+(require goblins/utils/simple-sealers)
+
 
 (define vat (make-vat))
 
@@ -14,7 +16,7 @@
             (author (or post-author "<<Anonymous>>"))
             (body (or post-body "<<Empty blogpost!>>")))
        (display
-         (format "~a\n~a\n By: ~a\n\n~a\n"
+        (format "~a\n~a\n By: ~a\n\n~a\n"
                 title title-underline author body))))))
 
 (define (display-blog-header blog-title)
@@ -23,7 +25,7 @@
   (define title-stars
     (make-string header-len #\*))
   (display
-   (format "~\n** ~a **\n~a\n"
+   (format "~a\n** ~a **\n~a\n"
            title-stars blog-title title-stars)))
 
 (define (display-post post)
@@ -40,21 +42,25 @@
      (display "\n"))
    ($ blog 'get-posts)))
 
+
+;; Interesting capability stuff below
+(require pk)
+
 (define (spawn-blog-and-admin title)
   (define posts
     (spawn ^cell '()))
   (define (^blog _bcom)
     (methods
      [(get-title) title]
-     [(get-posts) ($ posts 'get)]))
+     [(get-posts) ($ posts)]))
   (define (^admin bcom)
     (methods
      [(add-post post)
       (define current-posts
-        ($ posts 'get))
+        ($ posts))
       (define new-posts
         (cons post current-posts))
-      ($ posts 'set new-posts)]))
+      ($ posts new-posts)]))
   (define blog (spawn ^blog))
   (define admin (spawn ^admin))
   (values blog admin))
@@ -78,14 +84,154 @@
   (values post editor))
 
 
-(vat 'run
-     (lambda ()
-       (define-values (post editor)
-          (spawn-post-and-editor
-           #:title "A day in the Park"
-           #:author "Mikayla"
-           #:body "la-di-da..."))
+(define (spawn-post-and-editor-internal blog-sealer
+                                        #:title (title #f)
+                                        #:author (author #f)
+                                        #:body (body #f))
+  (define (^post _bcom)
+    (methods
+     [(get-content)
+      (define data-triple
+        ($ editor 'get-data))
+      (cons '*post* data-triple)]
+     [(get-sealed-editor)
+      (blog-sealer (list '*editor* editor))]
+     [(get-sealed-self)
+      (blog-sealer (list '*post-self-proof* post))]))
+  (define (^editor bcom title author body)
+    (methods
+     [(update #:title (title title) #:author (author author) #:body (body body))
+      (bcom (^editor bcom title author body))]
+     [(get-data)
+      (list title author body)]))
+  (define post (spawn ^post))
+  (define editor (spawn ^editor title author body))
+  (values post editor))
 
-        (display-post post)
 
-        ($ editor 'update #:body "it was a fine day...")))
+(define (new-spawn-blog-and-admin title)
+  (define-values (blog-seal blog-unseal blog-sealed?)
+    (make-sealer-triplet))
+  (define posts
+    (spawn ^cell '()))
+
+  (define (^blog _bcom)
+    (methods
+     [(get-title)
+      title]
+     [(get-posts)
+      ($ posts)]
+     ))
+
+  (define (^admin bcom)
+    (methods
+     [(new-post-and-editor
+       #:title (title #f)
+       #:author (author #f)
+       #:body (body #f))
+      (define-values (post editor)
+        (spawn-post-and-editor-internal
+         blog-seal
+         #:title title
+         #:author author
+         #:body body))
+      (list post editor)]
+     [(add-post post)
+      (define current-posts
+        ($ posts))
+      (define new-posts
+        (cons post current-posts))
+      (define post-self-proof
+        ($ post 'get-sealed-self))
+      (match (blog-unseal post-self-proof)
+        ((list '*post-self-proof* obj)
+         (unless (eq? obj post)
+           (error "Self-proof not for this post"))))
+      ($ posts new-posts)]
+
+     [edit-post
+      (make-keyword-procedure
+       (lambda (kws kw-vals post . args)
+         (define sealed-editor
+           ($ post 'get-sealed-editor))
+         (define editor
+           (match (blog-unseal sealed-editor)
+             ((list '*editor* editor) editor)))
+         (keyword-apply $ kws kw-vals editor 'update args)))]))
+
+  (values
+   (spawn ^blog)
+   (spawn ^admin)))
+
+
+(define (^logger _bcom)
+  (define log
+    (spawn ^cell '()))
+  (methods
+   [(append-to-log username object args)
+    (define new-log-entry
+      (list '*entry* 'user username 'object object 'args args))
+    (define current-log ($ log))
+    (define new-log (cons new-log-entry current-log))
+    ($ log new-log)]
+   [(get-log)
+    ($ log)]))
+
+(define (spawn-logged-revocable-proxy-pair username object log)
+  (define revoked?
+    (spawn ^cell #f))
+
+  (define (^proxy _bcom)
+    (make-keyword-procedure
+     (lambda (kws kw-vals . args)
+       (when ($ revoked?)
+         (error "Access revoked!"))
+       ($ log 'append-to-log username object args)
+       (keyword-apply $ kws kw-vals object args)
+       )))
+
+  (define proxy (spawn ^proxy))
+  (values proxy revoked?))
+
+
+;(define-values (post editor)
+;  (vat 'run
+;       (lambda ()
+;         (spawn-post-and-editor
+;          #:title "A day in the Park"
+;          #:author "Mikayla"
+;          #:body "la-di-da..."))
+;       ))
+
+(define-values (blog admin)
+  (vat 'run
+       (lambda ()
+         (new-spawn-blog-and-admin "Test blog!"))
+       ))
+
+(match-define (list test-post test-editor)
+  (vat 'run
+       (lambda ()
+         ($ admin 'new-post-and-editor
+            #:title "title"
+            #:author "TEST"
+            #:body "TEST TEST TEST"))
+       ))
+
+(define-vat-run vat-run vat)
+
+(define admin-log (vat-run (spawn ^logger)))
+
+(vat-run ($ admin 'add-post test-post))
+
+(define-values (admin-for-robert roberts-admin-revoked?)
+  (vat-run (spawn-logged-revocable-proxy-pair
+            "Robert"
+            admin
+            admin-log
+            )))
+
+; TODO:
+(vat-run ($ admin-for-robert 'edit-post test-post #:body "ROBERT-EDITED"))
+(vat-run (display-blog blog))
+(vat-run ($ admin-log 'get-log))
